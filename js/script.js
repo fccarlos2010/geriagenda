329193fd2cd3e3014a3a4c73085943a13a26dc04
FC Carlos PIO <pio2010@hotmail.com.br>
2026-09-01 15:39:39 -0300

    Implementa tela de Login

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("GeriAgenda iniciado com sucesso!");
+const loginForm = document.getElementById("loginForm");
+const mensagem = document.getElementById("mensagem");
+
+loginForm.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+
+    if (email === "" || senha === "") {
+
+        mensagem.textContent = "Preencha todos os campos.";
+
+        return;
+    }
+
+    mensagem.textContent = "Login realizado com sucesso!";
+});
